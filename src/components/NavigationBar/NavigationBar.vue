7afab4849af12b5980171ccfb0e12f5253988523
Daria Pionk <dariapionk13@gmail.com>
2024-11-11 14:44:46 +0100

    Redirecting on navbar

--- src/components/NavigationBar/NavigationBar.vue
+++ src/components/NavigationBar/NavigationBar.vue
@@ -1,14 +1,13 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand">🎬</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="#">Movies</a>
-      </div>
+      <div class="navbar-nav"><router-link to="/" class="nav-link">Home</router-link></div>
+      <div class="navbar-nav"><router-link to="/movies" class="nav-link">Movies</router-link></div>
     </div>
   </nav>
 </template>
